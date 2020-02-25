f9cfb84b66eba3700c0227f657a79ca116d91808
Gabriel Enrique Macho <gabe3195@gmail.com>
2020-02-25 06:13:30 +0100

    Capa de negocio

---
+++ BusinessLayer/AccountBL.cs
@@ -0,0 +1,42 @@
+﻿using DataLayer.Model;
+using DataLayer.Repository;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+
+
+namespace BusinessLayer
+{
+    public class AccountBL
+    {
+        public IdentityResult CreateAccount(Cliente persona)
+        {
+            IdentityResult result;
+            try
+            {
+                ClienteRepository clienteRepository = new ClienteRepository();
+                if (!clienteRepository.CheckEmail(persona.Email))
+                {
+                    clienteRepository.Create(persona);
+                    result = persona.Id != 0 ? IdentityResult.Success : new IdentityResult("No se ha creado el usuario");
+                }
+                else
+                {
+                    result = new IdentityResult("El email ya existe en otra cuenta de usuario");
+                }
+            }
+            catch(Exception ex)
+            {
+                result = new IdentityResult(ex.Message);
+
+            }
+            return result;
+        }
+        
+        public Cliente GetCliente (string email, string password)
+        {
+            ClienteRepository clienteRepository = new ClienteRepository();
+            return clienteRepository.Single(u => u.Email == email && u.Password == password);
+        }
+    }
+}
